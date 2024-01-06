3136c453f94d27710a15f0334ffa7920c9de52e2
Walter Bernal <wltrbm@gmail.com>
2024-01-05 21:16:53 -0700

    adjustment in scoring for types

--- src/utils/calculateTypeScores.js
+++ src/utils/calculateTypeScores.js
@@ -5,22 +5,26 @@ function calculateTypeScores(
   weakDefense,
   strongDefense,
   immuneDefense,
-  typesCount
+  typesCount,
+  weakerDefense,
+  strongerDefense
 ) {
   const effectiveCount = effectiveAttack.length
   const nonEffectiveCount = noneffectiveAttack.length
   const noEffectCount = noEffectAttack.length
   const normalCount =
     typesCount - effectiveCount - nonEffectiveCount - noEffectCount
-  const attackScore =
-    ((effectiveCount * 2 + nonEffectiveCount * 0.5 + normalCount) * 100) / 18
+  const attackScore = effectiveCount * 2 + normalCount + nonEffectiveCount * 0.5 - noEffectCount
 
   const weakCount = weakDefense.length
   const strongCount = strongDefense.length
   const immuneCount = immuneDefense.length
-  const standardCount = typesCount - weakCount - strongCount - immuneCount
-  const defenseScore =
-    ((immuneCount * 2 + strongCount * 1.5 + standardCount) * 100) / 18
+  const weakerCount = weakerDefense ? weakerDefense.length : 0
+  const strongerCount = strongerDefense ? strongerDefense.length : 0
+  const standardCount = typesCount - weakCount - weakerCount - strongCount - strongerCount - immuneCount
+  let defenseScore =
+    weakerCount * 4 + weakCount * 2 + standardCount + strongCount * 0.5 + strongerCount * 0.25
+  defenseScore = (defenseScore - typesCount) * -1 + typesCount
 
   const averageScore = (attackScore + defenseScore) / 2
 
